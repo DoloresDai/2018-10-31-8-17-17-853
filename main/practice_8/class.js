d88c11aa14a976d2d089ff6b2d42e97786d1e77c
841856753@qq.com <841856753@qq.com>
2018-12-24 05:49:28 +0800

    Take instance as a arguments

--- main/practice_8/class.js
+++ main/practice_8/class.js
@@ -1,1 +1,14 @@
-
+class Class = {
+  constructor(number) {
+    this.number = number;
+  }
+  getDisplayName() {
+    return Class + `${this.name}`;
+  }
+  assignLeader(student) {
+    student.klass.number === this.number ? this.leader = Tom;
+  }
+}
+export {
+  Class
+};
--- main/practice_8/student.js
+++ main/practice_8/student.js
@@ -1,1 +1,23 @@
-
+import {
+  Person
+} from '/person.js';
+import {
+  class
+} from '/class.js';
+class Student extends Person {
+  constructor(id, name, age, introduce, klass) {
+    super(name, age);
+    this.klass = new Class();
+  }
+  introduce() {
+      assignLeader(leader) === Tom ? Person.introduce() + `I am a Student.`
+      I am Leader of Class 2. `:Person.introduce() + `
+      I am a Student.I am at Class $ {
+        this.klass
+      }.
+      `;
+  }
+}
+export {
+  Student
+};
