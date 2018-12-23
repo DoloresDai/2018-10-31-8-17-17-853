61ff459b58d622de2e2dde359f20051d8956b7ed
841856753@qq.com <841856753@qq.com>
2018-12-24 05:45:48 +0800

    Multiple conditional judgements

--- main/practice_6/teacher.js
+++ main/practice_6/teacher.js
@@ -1,1 +1,15 @@
-
+import {
+  Person
+} from '/person.js';
+class Teacher extends Person {
+  constructor(name, age, introduce, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return this.class ? Person.introduce() + `I am a Teacher. I teach Class ${this.klass}.` : Person.introduce() + `I am a Teacher. I teach No Class.`;
+  }
+}
+export {
+  Teacher
+};
