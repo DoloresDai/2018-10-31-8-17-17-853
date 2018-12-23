c981790086a80d63a6cd0a2989b7132b0ab017d1
841856753@qq.com <841856753@qq.com>
2018-12-24 05:43:16 +0800

    Two inherits from a person class

--- main/practice_5/student.js
+++ main/practice_5/student.js
@@ -1,1 +1,15 @@
-
+import {
+  Person
+} from '/person.js';
+class Student extends Person {
+  constructor(name, age, introduce, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return (Person.introduce() + `I am a Student. I am at Class ${this.klass}.`);
+  }
+}
+export {
+  Student
+};
