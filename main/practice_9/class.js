f270777ce294577402d9738ab4b54100ff18d982
DoloresDai <841856753@qq.com>
2018-12-26 06:03:24 +0800

    Add appendMember function to judge student

--- main/practice_9/class.js
+++ main/practice_9/class.js
@@ -1,1 +1,15 @@
-
+ class Class = {
+   constructor(number) {
+     this.number = number;
+   }
+   getDisplayName() {
+     return Class + `${this.name}`;
+   }
+   assignLeader(student) {
+     student.klass.number === this.number ? this.leader = this.name : `It is not one of us.`;
+   }
+   appendMember(student) {
+     assignLeader(student);
+   }
+ }
+ module.exports = Class;
--- main/practice_9/person.js
+++ main/practice_9/person.js
@@ -1,1 +1,11 @@
-
+lass Person = {
+  constructor(id, name, age, introduce) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.introduce = function() {
+      return (`My name is ${this.name}.I am ${this.age} years old.`);
+    }
+  }
+}
+module.exports = Person;
