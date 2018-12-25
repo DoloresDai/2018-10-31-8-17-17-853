37624eb966ca32742d59f02c751efb284b4de4e6
DoloresDai <841856753@qq.com>
2018-12-26 07:23:32 +0800

    Add different interpretations of different states

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,18 @@
-
+ class Class = {
+   constructor(number) {
+     this.number = number;
+   }
+   getDisplayName() {
+     return Class + `${this.name}`;
+   }
+   assignLeader(student) {
+     student.klass.number === this.number ? `I am ${this.teacher.name}. I know ${this.leader.name} become Leader of Class ${this.number}.` : `It is not one of us.`;
+   }
+   appendMember(student, teacher) {
+     student.klass.number === this.number ? `I am ${this.teacher.name}. I know ${student.name} has joined Class ${this.number}.`
+   }
+   isIN() {
+     student.klass.number === this.number ? `I teach Class ${this.number}` : `I teach NO Class.`;
+   }
+ }
+ module.exports = Class;
